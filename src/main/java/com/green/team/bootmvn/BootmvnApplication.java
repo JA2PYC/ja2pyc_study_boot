cdbd26543846fe66e043ddf91313d060ecc3d5c4
gr2nade <gr2nade@gmail.com>
2025-01-04 23:12:39 +0900

    Initialize Spring Boot

---
+++ src/main/java/com/green/team/bootmvn/BootmvnApplication.java
@@ -0,0 +1,13 @@
+package com.green.team.bootmvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootmvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootmvnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/green/team/bootmvn/BootmvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.team.bootmvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootmvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
