d0a2fd5a1b8785a2eace66f2ced4979574e45895
gr2nade <gr2nade@gmail.com>
2025-01-12 23:17:30 +0900

    Python Application

--- src/main/java/com/green/team/bootmvn/BootmvnApplication.java
+++ src/main/java/com/green/team/bootmvn/BootmvnApplication.java
@@ -2,12 +2,17 @@ package com.green.team.bootmvn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BootmvnApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootmvnApplication.class, args);
+		ApplicationContext context = SpringApplication.run(BootmvnApplication.class, args);
+
+		PythonApplication pythonApplication = context.getBean(PythonApplication.class);
+		pythonApplication.runPython();
 	}
+
 
 }
---
+++ src/main/java/com/green/team/bootmvn/PythonApplication.java
@@ -0,0 +1,11 @@
+package com.green.team.bootmvn;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PythonApplication {
+
+    public void runPython() {
+        System.out.println("RunPython");
+    }
+}
